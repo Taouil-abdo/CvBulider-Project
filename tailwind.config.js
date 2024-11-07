bda019349b3fcbf3ee649c460bc596662b0ccd09
Taouil-abdo <abdotaouil03@gmail.com>
2024-11-07 18:04:03 +0100

    ajouter la structure et la partie hero section

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html","./asset/**/*.{css,js}"
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
